219d9c351b15f108555993f5c6a6beffe700daf5
ipetrash <gil9red@gmail.com>
2015-10-30 18:27:35 +0500

    Исправление ошибки, которая возникала во время запуска

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@
 from cx_Freeze import setup, Executable
 
 # TODO: http://cx-freeze.readthedocs.org/en/latest/distutils.html#distutils
+# TODO: с какой-то радости он добавляет левые библиотеки -- Qt, а я их не использую
 
 executables = [
     Executable('compress_image_fb2.py')
@@ -18,5 +19,11 @@ executables = [
 setup(name='compress_image_fb2',
       version='0.1',
       description='compress_image_fb2',
-      executables=executables
+      executables=executables,
+
+      # Иначе при запуске будет: ImportError: cannot import name _elementpath
+      options={
+          "build_exe": {"includes": ['lxml._elementpath']}
+      }
+
       )
